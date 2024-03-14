53687b2f344d0e7e434237564869e7dd6b7dba15
mborkowski29 <mikolajborkowski29@gmail.com>
2024-03-14 20:37:22 +0100

    dodano GetMax

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,6 +4,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("modyfikacja");
 Console.WriteLine("modyfikacja2");
 Console.WriteLine("modyfikacja3");
+Console.WriteLine("max wartosc");
 
 static double GetAverage(int[] numbers)
 {
@@ -20,3 +21,17 @@ static double GetAverage(int[] numbers)
 int[] numbers = { 1, 2, 3, 4 };
 
 double average=GetAverage(numbers);
+
+static double GetMax(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
